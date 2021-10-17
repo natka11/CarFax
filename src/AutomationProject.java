28a369f195acd131e328e571648b4fb1a5e968ce
Magda <magdarojs@gmail.com>
2021-10-17 17:26:34 -0400

    2nd Commit

--- src/AutomationProject.java
+++ src/AutomationProject.java
@@ -6,24 +6,30 @@ import org.openqa.selenium.By;
 
 import java.security.SecureRandom;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 public class AutomationProject {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //1. Navigate to http://duotifyapp.us-east-2.elasticbeanstalk.com/register.php
        System.setProperty("webdriver.chrome.driver","C:\\Users\\magda\\Desktop\\Selenium\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://duotifyapp.us-east-2.elasticbeanstalk.com/register.php");
+        Thread.sleep(1000);
 
 // 2. Verify the the title is "Welcome to Duotify!"
 
         String expectedTitle = "Welcome to Duotify!";
         String actualTitle = driver.getTitle();
         assertEquals(actualTitle,expectedTitle);
+        Thread.sleep(1000);
+
 
 // 3. Click on Signup here
         driver.findElement(By.id("hideLogin")).click();
+        Thread.sleep(1000);
 
 //4.  Fill out the form with the required info
      String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
@@ -49,6 +55,7 @@ public class AutomationProject {
         driver.findElement(By.id("email2")).sendKeys(email);
         driver.findElement(By.id("password")).sendKeys("Betty1234");
         driver.findElement(By.id("password2")).sendKeys("Betty1234");
+        Thread.sleep(1000);
 //5. Click on Sign up
         driver.findElement(By.name("registerButton")).click();
 
@@ -56,41 +63,53 @@ public class AutomationProject {
 //http://duotifyapp.us-east-2.elasticbeanstalk.com/browse.php?
         assertEquals(driver.getCurrentUrl(),
                 "http://duotifyapp.us-east-2.elasticbeanstalk.com/browse.php?");
+        Thread.sleep(1000);
 
  // 7. In the left navigation bar,verify that your first and last name matches
 // the first and last name that you used when signing up.
 
         String firstAndLastName = "Betty Smith";
-        String pageSource = driver.getCurrentUrl();
+        String pageSource = driver.getPageSource();
         assertTrue(pageSource.contains(firstAndLastName));
+        Thread.sleep(1000);
 //8.Click on the username on the left navigation bar and verify
 // the username on the main window is correct and then click logout.
 
+
         driver.findElement(By.id("nameFirstAndLast")).click();
+        Thread.sleep(1000);
         WebElement element = driver.findElement(By.className("userInfo"));
         assertEquals(element,"Betty Smith");
+        Thread.sleep(1000);
         driver.findElement(By.id("rafael")).click();
+        Thread.sleep(1000);
 
  //9.  Verify that you are logged out by verifying the URL is:
         //http://duotifyapp.us-east-2.elasticbeanstalk.com/register.php
 
         assertEquals(driver.getCurrentUrl(),
                 "http://duotifyapp.us-east-2.elasticbeanstalk.com/register.php");
+        Thread.sleep(1000);
 //10. Login using the same username and password when you signed up.
         driver.findElement(By.id("loginUsername")).sendKeys(username);
+        Thread.sleep(1000);
         driver.findElement(By.id("loginPassword")).sendKeys("Betty1234");
+        Thread.sleep(1000);
         driver.findElement(By.name("loginButton")).click();
+        Thread.sleep(1000);
 
 //11. Verify successful login by verifying that the home page contains
 // the text "You Might Also Like".
 
         String text = "You Might Also Like";
         String pageSource1 = driver.getCurrentUrl();
+        Thread.sleep(1000);
         assertTrue(pageSource1.contains(text));
 
 //12. Log out once again and verify that you are logged out.
 
         driver.findElement(By.id("rafael")).click();
+        Thread.sleep(1000);
 
         assertEquals(driver.getCurrentUrl(),
                 "http://duotifyapp.us-east-2.elasticbeanstalk.com/register.php");
